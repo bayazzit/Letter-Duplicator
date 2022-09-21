e6ddede4e956a23e4fe7555505f187ab6b80098f
Bahadır Bayazıt <97166522+bayazzit@users.noreply.github.com>
2022-09-21 13:05:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # TODO: Create auto-letters using letter.txt with the names using names.txt
+# If necessary, change the paths for the .txt files
 
-with open('Input/Names/names.txt', 'r') as inv_names_file:
+# Here you can change the path
+with open('names.txt', 'r') as inv_names_file:
     temp_name_list = inv_names_file.readlines()
 
 name_list = [name.replace('\n', '') for name in temp_name_list if '\n' in name]
 name_list.append(temp_name_list[-1])
 
-with open('Input/Letters/letter.txt', 'r') as letter_file:
+# Here you can change the path
+with open('letter.txt', 'r') as letter_file:
     letter_text_list = letter_file.readlines()
 
 letter_text = ''
@@ -14,6 +17,7 @@ for i in letter_text_list:
     letter_text += i
 
 for name in name_list:
-    with open(f'Output/ReadyToSend/letter_for_{name}.txt', 'x') as named_letter_file:
+    # Here the path is for the newly created letters, you can change the path if you want to create them in somewhere else
+    with open(f'ReadyToSend/letter_for_{name}.txt', 'x') as named_letter_file:
         named_letter_file.write(letter_text.replace('[name]', name))
 
